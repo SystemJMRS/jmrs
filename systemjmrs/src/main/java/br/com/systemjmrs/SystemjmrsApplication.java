df17c7ad4cf85aab4044fec357aa37e473339ad5
Rafael Lopes <31425443+Fuhking@users.noreply.github.com>
2017-11-18 19:48:28 -0200

    Original

--- systemjmrs/src/main/java/br/com/systemjmrs/SystemjmrsApplication.java
+++ systemjmrs/src/main/java/br/com/systemjmrs/SystemjmrsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-//Aleluia!!! \o/
 public class SystemjmrsApplication {
 
 	public static void main(String[] args) {
